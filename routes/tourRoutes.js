5bd195afbbf4395894630ede21942c4739f87bc4
yoga <11211086@student.itk.ac.id>
2023-09-23 21:26:53 +0800

    first commit

---
+++ routes/tourRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const tourController = require('../controllers/tourController')
+
+
+router.param('id', tourController.checkId);
+
+router
+  .route("/")
+  .get(tourController.getAllTours)
+  .post(
+    tourController.checkBody,
+    tourController.createTour)
+
+router
+  .route("/:id")
+  .get(tourController.getTourById)
+  .patch(tourController.editTour)
+  .delete(tourController.removeTour)
+
+module.exports = router;
---
+++ routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router()
+const userController = require('../controllers/userController')
+
+router
+  .route("/")
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
+
+router
+  .route("/:id")
+  .get(userController.getUserById)
+  .patch(userController.editUser)
+  .delete(userController.removeUser);
+
+module.exports = router;
